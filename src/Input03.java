b4694aa397e0caaf6cb3fb62e406b74b06128576
Kahlilwalker <Kahlilmyson@gmail.com>
2025-01-07 13:52:58 -0500

    pushing just so i can save

--- src/Input03.java
+++ src/Input03.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 class Input03 {
 
     public static void main(String[] args) {
         //Create a Scanner
-        
-        //Find and print the sum of three integers entered by the user
-        
-        
-        
-        
-        //Remember to close the Scanner
-        
+
+
+        Scanner scan = new Scanner(System.in);
+        int total = 0;
+
+        int count = 3;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.println("Enter integer " + i + ":");
+            total += scan.nextInt();
+        }
+
+        System.out.println("The sum is: " + total);
+        scan.close();
     }
-}
+        
+
+        //Remember to close the Scanner
+
+    }
+
